8439a48dda9f4ad95ad3bd17e211c9e312a72bd8
sl1pknot@abv.bg <sl1pknot@abv.bg>
2026-02-09 21:17:55 +0200

    implemented bubble, insertion, selection and merge sort, proceeding with quick probably

---
+++ deff1.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define n 10
+
+void merge_sort(int *array, int start, int end);
+void merging(int *array, int start, int mid, int end);
+
+int main(void) {
+
+    int array[n] = {9, 5, 7, 2, 0, 6, 3, 8, 1, 4};
+    int temp;
+    int position;
+    int j;
+
+    // Bubble sort:
+    // for (int i = 0; i < n - 1; i++) {
+    //     for (int j = 0; j < n-i-1; j++) {
+    //         if (array[j+1] < array[j]) {
+    //             temp = array[j+1];
+    //             array[j+1] = array[j];
+    //             array[j] = temp;
+    //         }
+    //     }       
+    // }
+
+    // for (int i = 0; i < n; i++) {
+    //     if (i == n-1) break;
+    //     position = i;
+    //     while (position > 0 && array[position+1] < array[position]) {
+    //         temp = array[position+1];
+    //         array[position+1] = array[position];
+    //         array[position] = temp;
+    //         position--;
+    //     }
+    // }
+    
+    // Insertion sort:
+    // for (int i = 1; i < n; i++) {
+    //     j = i;
+    //     temp = array[i];
+    //     while (j>=0 && temp < array[j-1]) {            
+    //         array[j] = array[j-1];  
+    //         j--;
+    //     }       
+    //     array[j] = temp;
+    // }
+
+    // Selection sort:
+    // for (int i = 0; i < n-1; i++) {
+    //     temp = array[i];
+    //     position = i;
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (temp > array[j]) {
+    //             temp = array[j];
+    //             position = j;
+    //         }
+    //     }
+    //     array[position] = array[i];
+    //     array[i] = temp;
+    // }
+
+    // Merge sort:
+    int len = sizeof(array)/sizeof(array[0]);
+
+    merge_sort(array, 0, len-1);
+
+    for (int i = 0; i < len; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void merge_sort(int *array, int start, int end) {
+    int mid = start + (end - start)/2;
+    if (start >= end) return;
+    merge_sort(array, start, mid);
+    merge_sort(array, mid+1, end);
+    merging(array, start, mid, end);    
+}
+
+void merging(int *array, int start, int mid, int end) {
+    int new_array[n];
+    int i = 0;
+    int j = start;
+    int k = mid + 1;
+
+    while (j <= mid && k <= end) {
+        new_array[i++] = array[j] < array[k] ? array[j++] : array[k++];
+    }
+    while (j <= mid) {
+        new_array[i++] = array[j++];
+    }
+    while (k <= end) {
+        new_array[i++] = array[k++];
+    }
+
+    memcpy(&array[start], new_array, (end - start + 1) * sizeof(int));
+}
